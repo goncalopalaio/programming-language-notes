f76101c137663431ea6179940ae414cba73563cc
goncalopalaio <goncalo.palaio@gmail.com>
2023-02-23 18:31:53 +0000

    Add gitignore

--- rust/second-season/notes/target/debug/deps/notes-95ab5f64b10b8f45.d
+++
@@ -1,5 +0,0 @@
-/Users/mb1/Documents/GitHub/programming-notes/rust/second-season/notes/target/debug/deps/notes-95ab5f64b10b8f45.rmeta: src/main.rs
-
-/Users/mb1/Documents/GitHub/programming-notes/rust/second-season/notes/target/debug/deps/notes-95ab5f64b10b8f45.d: src/main.rs
-
-src/main.rs:
--- rust/second-season/notes/target/debug/deps/notes-af4a9feca3a4b9e5.d
+++
@@ -1,5 +0,0 @@
-/Users/mb1/Documents/GitHub/programming-notes/rust/second-season/notes/target/debug/deps/notes-af4a9feca3a4b9e5.rmeta: src/main.rs
-
-/Users/mb1/Documents/GitHub/programming-notes/rust/second-season/notes/target/debug/deps/notes-af4a9feca3a4b9e5.d: src/main.rs
-
-src/main.rs:
