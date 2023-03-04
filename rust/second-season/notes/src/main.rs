3685473ed48798a82d047cd229d1be3e6accd937
goncalopalaio <goncalo.palaio@gmail.com>
2023-03-04 16:42:18 +0000

    Hashmaps

--- rust/second-season/notes/src/main.rs
+++ rust/second-season/notes/src/main.rs
@@ -13,6 +13,9 @@ fn main() {
     exercise_compound_types();
 
     if_else_conditions();
+
+    hashmaps();
+    exercise_hashmaps();
 }
 
 fn variables() {
@@ -311,6 +314,8 @@ fn exercise_compound_types() {
     let mut car: Car = car_factory(String::from(colors[0]), Transmission::Manual, false, 2000);
     let mut engine: Transmission = Transmission::Automatic;
 
+    println!("{:?}", car);
+
     // Car order #1: New, Manual, Hard top
     car = car_factory(String::from(colors[0]), engine, true, 0);
     println!(
@@ -355,6 +360,7 @@ fn if_else_conditions() {
     } else {
         out_of_range = true;
     }
+    println!("{:?}", out_of_range);
 
     let out_of_range = if num < 0 {
         false
@@ -365,4 +371,150 @@ fn if_else_conditions() {
     };
 
     println!("{}", out_of_range);
+}
+
+use std::collections::HashMap;
+fn hashmaps() {
+    let mut reviews: HashMap<String, String> = HashMap::new();
+    reviews.insert(
+        String::from("Ancient Roman History"),
+        String::from("Very accurate."),
+    );
+    reviews.insert(
+        String::from("Cooking with Rhubarb"),
+        String::from("Sweet recipes."),
+    );
+    reviews.insert(
+        String::from("Programming in Rust"),
+        String::from("Great examples."),
+    );
+
+    println!("{:?}", reviews.get("Programming in Rust"));
+    let obsolete_key = "Ancient Roman History";
+    let obsolete_value = reviews.remove(obsolete_key);
+    println!("{:?} {:?}", obsolete_key, obsolete_value);
+}
+
+fn exercise_hashmaps() {
+    #[derive(PartialEq, Debug)]
+    struct Car {
+        color: String,
+        motor: Transmission,
+        roof: bool,
+        age: (Age, u32),
+    }
+
+    #[derive(PartialEq, Debug)]
+    enum Transmission {
+        Manual,
+        SemiAuto,
+        Automatic,
+    }
+
+    #[derive(PartialEq, Debug)]
+    enum Age {
+        New,
+        Used,
+    }
+
+    // Get the car quality by testing the value of the input argument
+    // - miles (u32)
+    // Return tuple with car age ("New" or "Used") and mileage
+    fn car_quality(miles: u32) -> (Age, u32) {
+        // Check if car has accumulated miles
+        // Return tuple early for Used car
+        if miles > 0 {
+            return (Age::Used, miles);
+        }
+
+        // Return tuple for New car, no need for "return" keyword or semicolon
+        (Age::New, miles)
+    }
+
+    // Build "Car" using input arguments
+    fn car_factory(order: i32, miles: u32) -> Car {
+        let colors = ["Blue", "Green", "Red", "Silver"];
+
+        // Prevent panic: Check color index for colors array, reset as needed
+        // Valid color = 1, 2, 3, or 4
+        // If color > 4, reduce color to valid index
+        let mut color = order as usize;
+        if color > 4 {
+            // color = 5 --> index 1, 6 --> 2, 7 --> 3, 8 --> 4
+            color = color - 4;
+        }
+
+        // Add variety to orders for motor type and roof type
+        let mut motor = Transmission::Manual;
+        let mut roof = true;
+        if order % 3 == 0 {
+            // 3, 6, 9
+            motor = Transmission::Automatic;
+        } else if order % 2 == 0 {
+            // 2, 4, 8, 10
+            motor = Transmission::SemiAuto;
+            roof = false;
+        } // 1, 5, 7, 11
+
+        // Return requested "Car"
+        Car {
+            color: String::from(colors[(color - 1) as usize]),
+            motor: motor,
+            roof: roof,
+            age: car_quality(miles),
+        }
+    }
+
+    // Initialize counter variable
+    let mut order = 1;
+    // Declare a car as mutable "Car" struct
+    let mut car: Car;
+
+    // Order 6 cars, increment "order" for each request
+    // Car order #1: Used, Hard top
+    car = car_factory(order, 1000);
+    println!(
+        "{}: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        order, car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
+
+    // Car order #2: Used, Convertible
+    order = order + 1;
+    car = car_factory(order, 2000);
+    println!(
+        "{}: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        order, car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
+
+    // Car order #3: New, Hard top
+    order = order + 1;
+    car = car_factory(order, 0);
+    println!(
+        "{}: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        order, car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
+
+    // Car order #4: New, Convertible
+    order = order + 1;
+    car = car_factory(order, 0);
+    println!(
+        "{}: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        order, car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
+
+    // Car order #5: Used, Hard top
+    order = order + 1;
+    car = car_factory(order, 3000);
+    println!(
+        "{}: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        order, car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
+
+    // Car order #6: Used, Hard top
+    order = order + 1;
+    car = car_factory(order, 4000);
+    println!(
+        "{}: {:?}, Hard top = {}, {:?}, {}, {} miles",
+        order, car.age.0, car.roof, car.motor, car.color, car.age.1
+    );
 }
--- rust/second-season/notes/target/debug/notes.d
+++ rust/second-season/notes/target/debug/notes.d
@@ -1,1 +1,1 @@
-/Users/goncalopalaio/Documents/GitHub/programming-notes/rust/second-season/notes/target/debug/notes: /Users/goncalopalaio/Documents/GitHub/programming-notes/rust/second-season/notes/src/main.rs
+/Users/mb1/Documents/GitHub/programming-notes/rust/second-season/notes/target/debug/notes: /Users/mb1/Documents/GitHub/programming-notes/rust/second-season/notes/src/main.rs
